5b811c86a7415f4f506a37a425e74831fae8cacf
06kellyjac <06kellyjac@googlemail.com>
2018-09-09 23:41:45 +0100

    Wasn't using the response from the api.

--- .scripts/poll_node_docker_hub.sh
+++ .scripts/poll_node_docker_hub.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 set -eu
-DOCKER_API_URL="https://hub.docker.com/v2/repositories/library/node/"
-RESULT=$(wget -qO- $DOCKER_API_URL)
-
 UPDATE_FILE="UPDATED"
 
-if [ "$RESULT" = "OK" ]; then
-	echo "API response recived successfully"
-else
-	echo "ERROR: API response not recived successfully"
-	echo "$RESULT"
-fi
+DOCKER_API_URL="https://hub.docker.com/v2/repositories/library/node/"
 
 DATE_TIME_REGEX='"last_updated": "[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{6}Z"'
 
 GIT_LAST_UPDATED=$(cat "$UPDATE_FILE")
-DOCKER_HUB_LAST_UPDATED=$(grep -oE "$DATE_TIME_REGEX" | cut -d'"' -f4)
+DOCKER_HUB_LAST_UPDATED=$(wget -qO- $DOCKER_API_URL | grep -oE "$DATE_TIME_REGEX" | cut -d'"' -f4)
 
 echo "Docker Hub Node Image update date-time: $DOCKER_HUB_LAST_UPDATED"
 echo "Git Node Image update date-time: $GIT_LAST_UPDATED"
