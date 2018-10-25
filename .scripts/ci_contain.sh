4b9b0d4bd22c914b57c0f2eb67cf0863121cbff5
06kellyjac <06kellyjac@googlemail.com>
2018-10-25 23:08:15 +0100

    Fixed shellcheck issues

--- .scripts/ci_contain.sh
+++ .scripts/ci_contain.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 DEFAULT_BASE="alpine"
 
 # Login
-docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+docker login -u "${CI_REGISTRY_USER}" -p "${CI_REGISTRY_PASSWORD}" "${CI_REGISTRY}"
 
 set -x
 
@@ -13,22 +13,22 @@ set -x
 #                      ^^^^^^                     ^^^^^^                     ^^^^^^^^^^^
 CI_JOB_NAME_END=$(echo "${CI_JOB_NAME}" | cut -d':' -f3)
 
-function generate_image_tag () {
+generate_image_tag () {
 	echo "${CI_REGISTRY_IMAGE}:${CI_JOB_NAME_END}${1-}"
 }
 
 IMAGE_NAME="$(generate_image_tag)"
 
-if [[ ! $(echo "$CI_JOB_NAME_END" | grep "-") ]]; then
+if echo "$CI_JOB_NAME_END" | grep "-"; then
+	TARGET_DIRECTORY=$(echo "$CI_JOB_NAME_END" | awk -F'-' '{print $2"/"$1}')
+else
 	TAG=$(generate_image_tag "-${DEFAULT_BASE}")
 	IMAGE_NAME="$IMAGE_NAME $TAG"
 	TARGET_DIRECTORY="${DEFAULT_BASE}/${CI_JOB_NAME_END}"
-else
-	TARGET_DIRECTORY=$(echo "$CI_JOB_NAME_END" | awk -F'-' '{print $2"/"$1}')
 fi
 
-DOCKER_BUILD_LIST=$(for IMAGE in $IMAGE_NAME; do printf -- "-t $IMAGE "; done)
-docker build $DOCKER_BUILD_LIST $TARGET_DIRECTORY
+DOCKER_BUILD_LIST=$(for IMAGE in $IMAGE_NAME; do printf -- "-t %s " "$IMAGE"; done)
+docker build "$DOCKER_BUILD_LIST" "$TARGET_DIRECTORY"
 
 for IMAGE in $IMAGE_NAME; do
 	docker push "$IMAGE"
