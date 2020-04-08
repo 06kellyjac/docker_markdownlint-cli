45be8b27a4108d7a8bc2338ba72dda5002a86e59
06kellyjac <dev@j-k.io>
2020-04-08 17:55:58 +0100

    Ammended ci_contain.sh for new Dockerfile locations

--- scripts/ci_contain.sh
+++ scripts/ci_contain.sh
@@ -17,16 +17,16 @@ generate_image_tag () { echo "${CI_REGISTRY_IMAGE}:${CI_JOB_NAME_END}${1-}"; }
 IMAGE_NAME="$(generate_image_tag)"
 
 if echo "$CI_JOB_NAME_END" | grep "-"; then
-	TARGET_DIRECTORY=$(echo "$CI_JOB_NAME_END" | awk -F'-' '{print $2"/"$1}')
+	TARGET=$(echo "$CI_JOB_NAME_END" | awk -F'-' '{print $2" -f Dockerfile."$1}')
 else
 	TAG=$(generate_image_tag "-${DEFAULT_BASE}")
 	IMAGE_NAME="$IMAGE_NAME $TAG"
-	TARGET_DIRECTORY="${DEFAULT_BASE}/${CI_JOB_NAME_END}"
+	TARGET="${DEFAULT_BASE} -f Dockerfile.${CI_JOB_NAME_END}"
 fi
 
 DOCKER_BUILD_LIST=$(for IMAGE in $IMAGE_NAME; do printf -- "-t %s " "$IMAGE"; done)
 # shellcheck disable=SC2086
-docker build $DOCKER_BUILD_LIST "$TARGET_DIRECTORY"
+docker build $DOCKER_BUILD_LIST "$TARGET"
 
 for IMAGE in $IMAGE_NAME; do
 	docker push "$IMAGE"
